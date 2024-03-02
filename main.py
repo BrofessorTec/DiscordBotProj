dd77949acf882e25c899b7f848d0e72ab8263248
BrofessorTec <125778428+BrofessorTec@users.noreply.github.com>
2024-03-02 16:16:36 -0500

    /roll and /help commands working

--- main.py
+++ main.py
@@ -11,4 +11,44 @@ import random
 # start of code here
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-print(TOKEN)
+
+
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('No Message')
+        return
+    
+    try:
+        response: str = get_response(user_message)
+        await message.channel.send(response)
+    except Exception as error:
+        print(error)
+
+    
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is running')
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return   
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: {user_message}')
+    await send_message(message, user_message)
+
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
--- responses.py
+++ responses.py
@@ -1,2 +1,22 @@
+from random import choice, randint
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError('Still need to write this')
+    lowered: str = user_input.lower()
+    print(lowered)
+
+    if lowered == '':
+        return 'Well hello!'
+    elif lowered[0] != '/':
+        return
+    elif lowered == '/help':
+        return '/roll to roll a d20\n/roll [Size] to roll a die of the given size'
+    elif '/roll ' in lowered:
+        loweredTrim: int = int(lowered[6:])
+        print(loweredTrim)
+        loweredTrimNum = str(randint(1,loweredTrim))
+        print(loweredTrimNum)
+        return f'You rolled a {loweredTrimNum} out of {str(loweredTrim)}!'
+    elif '/roll' in lowered:
+        return f'You rolled a {randint(1,20)} out of 20!'
+    else:
+        return f'There was an error.'
