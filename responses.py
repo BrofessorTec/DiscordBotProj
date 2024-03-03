2482df9518a319368e5adfc285f77a8d614e25c2
BrofessorTec <125778428+BrofessorTec@users.noreply.github.com>
2024-03-03 00:15:43 -0500

    Tested some code for deathroll
    Currently need to figure out how to save the previous deathroll run as a global variable

--- responses.py
+++ responses.py
@@ -1,7 +1,17 @@
 from random import choice, randint
 
+deathrollStarted = None
+
+def set_deathrollStarted():
+    deathrollStarted = True
+
+def get_deathrollStarted():
+    return deathrollStarted
+
+
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
+
     print(lowered)
 
     if lowered == '':
@@ -18,5 +28,47 @@ def get_response(user_input: str) -> str:
         return f'You rolled a {loweredTrimNum} out of {str(loweredTrim)}!'
     elif '/roll' in lowered:
         return f'You rolled a {randint(1,20)} out of 20!'
+    elif '/deathroll ' in lowered:
+        deathrollTrim: int = int(lowered[11:])
+        print(deathrollTrim)
+        deathrollTrimNum = str(randint(1,deathrollTrim))
+        print(deathrollTrimNum)
+        if (deathrollTrimNum == 1):
+            deathrollTrimNum = 0
+            return f'You rolled a 1! You lose Deathroll!'
+        deathrollCont = deathrollTrim
+
+        return f'You rolled a {deathrollTrimNum} out of {deathrollTrim}! Use "/deathroll" to continue!'
+    elif '/deathroll' in lowered:
+        if 'deathrollCont' in locals() and deathrollCont is None:
+            deathrollCont = 999
+            print(deathrollCont)
+        deathrollTrimNum = str(randint(1,deathrollCont))
+        print(deathrollTrimNum)
+        if (deathrollTrimNum == 1):
+            deathrollTrimNum = 0
+            return f'You rolled a 1! You lose Deathroll!'
+        return f'You rolled a {deathrollTrimNum} out of {deathrollCont}! Use "/deathroll" to continue!'
     else:
         return f'There was an error.'
+    
+
+
+def deathroll():
+    deathrollCont = 999
+    deathrollTrimNum = str(randint(1,deathrollCont))
+    print(deathrollTrimNum)
+    if (deathrollTrimNum == 1):
+        deathrollTrimNum = 0
+        return f'You rolled a 1! You lose Deathroll!'
+    return f'You rolled a {deathrollTrimNum} out of {deathrollCont}! Use "/deathroll" to continue!'
+
+def deathroll(maxNum):
+    deathrollCont = maxNum
+    deathrollTrimNum = str(randint(1,deathrollCont))
+    print(deathrollTrimNum)
+    if (deathrollTrimNum == 1):
+        deathrollTrimNum = 0
+        return f'You rolled a 1! You lose Deathroll!'
+    return f'You rolled a {deathrollTrimNum} out of {deathrollCont}! Use "/deathroll" to continue!'
+
