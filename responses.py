ea495284c9572afdf5975ecf9507361e973755d8
BrofessorTec <125778428+BrofessorTec@users.noreply.github.com>
2024-03-03 00:57:21 -0500

    Fixed deathroll and updated help

--- responses.py
+++ responses.py
@@ -1,22 +1,14 @@
 from random import choice, randint
 
-deathrollStarted = None
-
-def set_deathrollStarted():
-    global deathrollStarted
-    deathrollStarted = True
-
 deathrollCont = 0
 deathrollContHolder = 0
-
-def get_deathrollStarted():
-    return deathrollStarted
-
+deathrollCurrent = 0
 
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
     global deathrollCont
     global deathrollContHolder
+    global deathrollCurrent
 
     print(lowered)
 
@@ -25,7 +17,9 @@ def get_response(user_input: str) -> str:
     elif lowered[0] != '/':
         return
     elif lowered == '/help':
-        return '/roll to roll a d20\n/roll [Size] to roll a die of the given size'
+        helpStr = '/roll to roll a d20\n/roll [Size] to roll a die of the given size\n/deathroll [maxNum] to start a deathroll game with the given maximum\n/deathroll to continue a deathroll game or start a new one from 999'
+        # someone help me format this help string better lol. idk how to get it on multiple lines and still be one string
+        return helpStr
     elif '/roll ' in lowered:
         loweredTrim: int = int(lowered[6:])
         print(loweredTrim)
@@ -42,49 +36,20 @@ def get_response(user_input: str) -> str:
         if (deathrollTrimNum == 1):
             deathrollTrimNum = 0
             return f'You rolled a 1! You lose Deathroll!'
-        deathrollCont = deathrollTrim
-
+        deathrollCont = deathrollTrimNum
         return f'You rolled a {deathrollTrimNum} out of {deathrollTrim}! Use "/deathroll" to continue!'
     elif '/deathroll' in lowered:
         if deathrollCont == 0:
             deathrollCont = 999
-            print(deathrollCont)
-        if deathrollContHolder == 0:
-            deathrollTrimNum = str(randint(1,deathrollCont))
-            print(deathrollTrimNum)
-            deathrollContHolder = deathrollTrimNum
-            if (deathrollTrimNum == 1):
-                deathrollTrimNum = 0
-                return f'You rolled a 1! You lose Deathroll!'
-            return f'You rolled a {deathrollTrimNum} out of {deathrollCont}! Use "/deathroll" to continue!'
-        else:
-            deathrollTrimNum = str(randint(1,deathrollCont))
-            print(deathrollTrimNum)
-            deathrollContHolder = deathrollTrimNum
-            if (deathrollTrimNum == 1):
-                deathrollTrimNum = 0
-                return f'You rolled a 1! You lose Deathroll!'
-            return f'You rolled a {deathrollTrimNum} out of {deathrollCont}! Use "/deathroll" to continue!'            
+            print(deathrollCont) 
+        deathrollCurrent = str(randint(1,int(deathrollCont)))
+        deathrollContHolder = str(deathrollCont)
+        deathrollCont = deathrollCurrent
+        print(deathrollCurrent)
+        if (int(deathrollCurrent) == int(1)):
+            deathrollCont = 0
+            print('loser')
+            return f'You rolled a 1! You lose Deathroll!'
+        return f'You rolled a {str(deathrollCurrent)} out of {str(deathrollContHolder)}! Use "/deathroll" to continue!'  
     else:
         return f'There was an error.'
-    
-
-
-def deathroll():
-    deathrollCont = 999
-    deathrollTrimNum = str(randint(1,deathrollCont))
-    print(deathrollTrimNum)
-    if (deathrollTrimNum == 1):
-        deathrollTrimNum = 0
-        return f'You rolled a 1! You lose Deathroll!'
-    return f'You rolled a {deathrollTrimNum} out of {deathrollCont}! Use "/deathroll" to continue!'
-
-def deathroll(maxNum):
-    deathrollCont = maxNum
-    deathrollTrimNum = str(randint(1,deathrollCont))
-    print(deathrollTrimNum)
-    if (deathrollTrimNum == 1):
-        deathrollTrimNum = 0
-        return f'You rolled a 1! You lose Deathroll!'
-    return f'You rolled a {deathrollTrimNum} out of {deathrollCont}! Use "/deathroll" to continue!'
-
